c3100dcffcb42c638029af76aed4c1e59e081925
vaishnav eega <130209386+vaishnavv04@users.noreply.github.com>
2025-02-15 16:14:09 +0530

    Time: 728 ms (5.02%) | Memory: 44.1 MB (15.20%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        if(grid[0][0]==1)
+        if(grid[0][0]==1 || grid[n-1][n-1])
         return -1;
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
-        pq.push({0,{0,0}});
+        queue<pair<int,int>> q;
+        q.push({0,0});
+        int lvl = 0;
         grid[0][0] = -1;
-        while(!pq.empty())
+        while(!q.empty())
         {
-            int k = pq.size();
+            int k = q.size();
             while(k--)
             {
-                auto it = pq.top();
-                int dist = it.first;
-                int x = it.second.first , y = it.second.second;
-                // cout<<x<<" "<<y<<endl;
+                int x = q.front().first , y = q.front().second;
+                cout<<x<<" "<<y<<endl;
                 if(x==n-1 && y==n-1)
-                return dist+1;
-                pq.pop();
+                return lvl+1;
+                q.pop();
                 vector<int> dx = {-1,-1,-1,0,0,1,1,1} ;
                 vector<int> dy = {1,0,-1,1,-1,1,0,-1} ;
                 for(int i=0;i<8;i++)
@@ -26,12 +25,15 @@ public:
                     int nx = x+dx[i] , ny = y+dy[i];
                     if(nx>=0 && nx<n && ny>=0 && ny<n && grid[nx][ny]==0)
                     {
-                        pq.push({dist+1,{nx,ny}});
+                        if(nx==n-1 && ny==n-1)
+                        return lvl+2;
+                        q.push({nx,ny});
                         grid[nx][ny] = -1;
                         // cout<<nx<<" "<<ny<<endl;
                     }
                 }
             }
+            lvl++;
         }
         return -1;
     }
