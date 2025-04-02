3d8d7efdb5b6c6632e65f633ec193687d5bccc24
vaishnav eega <130209386+vaishnavv04@users.noreply.github.com>
2025-04-02 10:31:38 +0530

    Time: 1 ms (42.77%) | Memory: 21.5 MB (88.55%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
-        int n = nums.size();
+        int n  = nums.size();
         long long ans = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                for (int k = j + 1; k < n; ++k) {
-                    long long diff = static_cast<long long>(nums[i]) - static_cast<long long>(nums[j]);
-                    long long product = diff * static_cast<long long>(nums[k]);
-                    ans = max(ans, product);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                for(int k=j+1;k<n;k++)
+                {
+                    ans = max(ans,(long long)(nums[i]-nums[j])*nums[k]);
                 }
             }
         }
